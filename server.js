f02733d825e9645771167744ebc5e4d964320e79
s225107484 <s225107484@deakin.edu.au>
2025-09-03 22:56:08 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
 
-// serve static client files from /public
 app.use(express.static('public'));
 
-// IMPORTANT: create http server from app and pass to socket.io
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// socket logic
 io.on('connection', (socket) => {
   console.log('a user connected');
 
@@ -16,13 +13,11 @@ io.on('connection', (socket) => {
     console.log('user disconnected');
   });
 
-  // send a random number every second
   setInterval(() => {
     socket.emit('number', parseInt(Math.random() * 10));
   }, 1000);
 });
 
-// start server
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
