0532c5c15430a6f12d6fc264393eea7c416b35b7
s225107484 <s225107484@deakin.edu.au>
2025-09-03 22:55:11 +1000

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,5 @@
-// connect to socket server
 const socket = io();
 
-// listen for the 'number' event and display it
 socket.on('number', (msg) => {
   console.log('Random number:', msg);
   document.getElementById('number').innerText = msg;
